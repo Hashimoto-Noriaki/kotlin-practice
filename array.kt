3f16821962a00b0e1f3979e29220941989cd10d3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-04-29 02:48:15 +0900

    booleanと配列

---
+++ array.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val animal = listOf("犬", "猫", "猿")
+    println(animal[0])
+    println(animal[1])
+
+    //犬
+    //猫
+}
+//listOf はKotlinの標準ライブラリにある「リストを作る関数」
+//listOfはイミュータブル(変更できない)
---
+++ boolean.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val var01:Int = 100
+    val var02:Int = 200
+    val var_bool:Boolean
+    val var03:Int = 1000
+    val var04:Int = 2000
+    val var_bool2:Boolean
+
+    var_bool = (var01 < var02)
+    println(var_bool)
+    //true
+    
+    var_bool2 = (var03 > var04);
+    println(var_bool2);
+    //false
+}
