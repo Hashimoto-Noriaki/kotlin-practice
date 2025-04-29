7f8a434586c2252116c41b06350579de168c403a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-04-29 22:47:41 +0900

    if文

---
+++ if.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val age = 18
+
+    if (age >= 20) {
+        println("adult")
+    } else if (age == 0) {
+        println("baby")
+    } else {
+        println("child")
+    }
+}
+//child
